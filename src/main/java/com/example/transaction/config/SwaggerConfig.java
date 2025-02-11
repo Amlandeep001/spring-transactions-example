01c90c5fc1e2c12c7686e779e14a4c3a2c6fca09
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2025-02-12 02:35:14 +0530

    Added version info to SwaggerConfig

--- src/main/java/com/example/transaction/config/SwaggerConfig.java
+++ src/main/java/com/example/transaction/config/SwaggerConfig.java
@@ -3,7 +3,7 @@ package com.example.transaction.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@OpenAPIDefinition(info = @Info(title = "Spring Boot Transactions Demo Application", description = "Api documentation"))
+@OpenAPIDefinition(info = @Info(title = "Spring Boot Transactions Demo Application", description = "Api documentation", version = "1.0"))
 public class SwaggerConfig
 {
 }
